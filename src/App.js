1801b97aa3e851cf4986282910825b8aa2eaf148
meghamodi <meghamodi007@gmail.com>
2023-08-09 16:33:05 -0500

    intialize and add Header component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Header />
+      </Router>
+    </div>
+  )
+}
+
+export default App
